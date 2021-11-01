0c0d6b404d9fcf7e134957a21a6b8a00cb79197f
jgagne <justin.gagne@montserrat.edu>
2021-11-01 16:57:47 -0400

    Update prototypes content
    - Update navigation headings
    - Add back sticky navigation
    - Remove card topics, faux tags
    - Update JS, minor cleanup, not DRY

--- dev/prototypes/courses/js/main.js
+++ dev/prototypes/courses/js/main.js
@@ -1,39 +1,41 @@
+// Format toggle
+
 var toggleFull = document.querySelector('#toggle-full-ui');
-toggleFull.addEventListener('click', function (event) {
+toggleFull.addEventListener('click', function() {
   if (document.querySelector("#toggle-full-ui").checked == true) {
     document.querySelector("#toggle-full").checked = true;
   } else {
     document.querySelector("#toggle-full").checked = false;
-  };
+  }
 });
 
 var toggleShort = document.querySelector('#toggle-short-ui');
-toggleShort.addEventListener('click', function (event) {
+toggleShort.addEventListener('click', function() {
   if (document.querySelector("#toggle-short-ui").checked == true) {
     document.querySelector("#toggle-short").checked = true;
   } else {
     document.querySelector("#toggle-short").checked = false;
-  };
+  }
 });
 
 var toggleTutorial = document.querySelector('#toggle-tutorial-ui');
-toggleTutorial.addEventListener('click', function (event) {
+toggleTutorial.addEventListener('click', function() {
   if (document.querySelector("#toggle-tutorial-ui").checked == true) {
     document.querySelector("#toggle-tutorial").checked = true;
   } else {
     document.querySelector("#toggle-tutorial").checked = false;
-  };
+  }
 });
 
-// Courses Refresh
+// Courses refresh
 
 [].forEach.call(document.querySelectorAll('.courses-refresh'), function(refresh) {
   refresh.addEventListener('click', function() {
-    document.querySelector("#toggle-full").checked = true;
-    document.querySelector("#toggle-short").checked = true;
-    document.querySelector("#toggle-tutorial").checked = true;
     document.querySelector("#toggle-full-ui").checked = true;
     document.querySelector("#toggle-short-ui").checked = true;
     document.querySelector("#toggle-tutorial-ui").checked = true;
+    document.querySelector("#toggle-full").checked = true;
+    document.querySelector("#toggle-short").checked = true;
+    document.querySelector("#toggle-tutorial").checked = true;
   })
 });
