7bd60870715c7e097d6f0dd28062fb622276ec98
jgagne <justin.gagne@montserrat.edu>
2022-02-17 10:19:56 -0500

    Update sort option
    - Remove topics navigation list
    - Add Skill Level sort
    - Add Publish Date sort (work-in-progress)
    - Remove Popularity sort
    - Update list structure
    - Remove third-party script

--- dev/prototypes/courses/version-2/js/main.js
+++ dev/prototypes/courses/version-2/js/main.js
@@ -1,57 +1,66 @@
-// Sort (placeholder)
+// Sort
 
-var options = {
-  valueNames: [ "level", "rank" ]
+// Level
+
+var selectLevel = document.querySelector('#select-level');
+selectLevel.onchange = function() {
+  document.querySelector('#catalog').className = 'courses ' + this.value;
 };
 
-var List = new List("courses", options);
+// Order
+
+var selectOrder = document.querySelector('#select-order');
+selectOrder.onchange = function() {
+  document.querySelector('#catalog').className = 'courses ' + this.value;
+};
+
 
 // Format toggle
 
-var toggleFull = document.querySelector("#toggle-full-ui");
-toggleFull.addEventListener("click", function() {
-  if (document.querySelector("#toggle-full-ui").checked == true) {
-    document.querySelector("#toggle-full").checked = true;
+var toggleFull = document.querySelector('#toggle-full-ui');
+toggleFull.addEventListener('click', function() {
+  if (document.querySelector('#toggle-full-ui').checked == true) {
+    document.querySelector('#toggle-full').checked = true;
   } else {
-    document.querySelector("#toggle-full").checked = false;
+    document.querySelector('#toggle-full').checked = false;
   }
 });
 
-var toggleShort = document.querySelector("#toggle-short-ui");
-toggleShort.addEventListener("click", function() {
-  if (document.querySelector("#toggle-short-ui").checked == true) {
-    document.querySelector("#toggle-short").checked = true;
+var toggleShort = document.querySelector('#toggle-short-ui');
+toggleShort.addEventListener('click', function() {
+  if (document.querySelector('#toggle-short-ui').checked == true) {
+    document.querySelector('#toggle-short').checked = true;
   } else {
-    document.querySelector("#toggle-short").checked = false;
+    document.querySelector('#toggle-short').checked = false;
   }
 });
 
-var toggleTutorial = document.querySelector("#toggle-tutorial-ui");
-toggleTutorial.addEventListener("click", function() {
-  if (document.querySelector("#toggle-tutorial-ui").checked == true) {
-    document.querySelector("#toggle-tutorial").checked = true;
+var toggleTutorial = document.querySelector('#toggle-tutorial-ui');
+toggleTutorial.addEventListener('click', function() {
+  if (document.querySelector('#toggle-tutorial-ui').checked == true) {
+    document.querySelector('#toggle-tutorial').checked = true;
   } else {
-    document.querySelector("#toggle-tutorial").checked = false;
+    document.querySelector('#toggle-tutorial').checked = false;
   }
 });
 
 // Course card disclosure
 
-[].forEach.call(document.querySelectorAll(".info"), function(expanded) {
-  expanded.addEventListener("toggle", function() {
-    expanded.parentElement.parentElement.classList.toggle("expanded");
+[].forEach.call(document.querySelectorAll('.info'), function(expanded) {
+  expanded.addEventListener('toggle', function() {
+    expanded.parentElement.parentElement.classList.toggle('expanded');
   })
 });
 
 // Courses refresh
 
-[].forEach.call(document.querySelectorAll(".courses-refresh"), function(refresh) {
-  refresh.addEventListener("click", function() {
-    document.querySelector("#toggle-full-ui").checked = true;
-    document.querySelector("#toggle-short-ui").checked = true;
-    document.querySelector("#toggle-tutorial-ui").checked = true;
-    document.querySelector("#toggle-full").checked = true;
-    document.querySelector("#toggle-short").checked = true;
-    document.querySelector("#toggle-tutorial").checked = true;
+[].forEach.call(document.querySelectorAll('.courses-refresh'), function(refresh) {
+  refresh.addEventListener('click', function() {
+    document.querySelector('#toggle-full-ui').checked = true;
+    document.querySelector('#toggle-short-ui').checked = true;
+    document.querySelector('#toggle-tutorial-ui').checked = true;
+    document.querySelector('#toggle-full').checked = true;
+    document.querySelector('#toggle-short').checked = true;
+    document.querySelector('#toggle-tutorial').checked = true;
   })
 });
