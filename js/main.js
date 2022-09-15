a492ac9d683207d0fe1cbfed4b639d747e86ec2e
Roman Edirisinghe <rediris@users.noreply.github.com>
2022-09-15 19:25:56 -0400

    Add files from original branch
    via
    https://github.com/gymnasium/gymcms/pull/796
    
    ## What this PR Does:
    - (in general, this PR is an early stage in the the process of streamlining browser requests in order to reduce loading time)
    - improved meta tags for preload/prefetch
    - some open edx JS files have been added to the `_includes/js/vendors/` directory. For the most part, these files can safely be ignored for this PR, as they are not doing anything yet.
    - combine job module CSS and JS with main CSS/JS files
    - combine some of our JS files (see `js/main.js` (the cookiebanner.js being an exception, and there are reasons for this).

---
+++ js/main.js
@@ -0,0 +1,20 @@
+---
+---
+{%comment%}
+Here, we are combining all of our JS files to reduce requests
+TODO: audit these to figure out which we need to load, and where. For example, the prism.js file is only used on quiz/exam pages. Maybe we create an auth.js file, which loads the resources needed for logged in users? Alternately, we pass that file back to the theme.
+{%endcomment%}
+
+{% include js/gymnasium.js %}
+{%comment%}
+  Temporarily commented out (until we update the theme)
+  {% include js/vendor/openedx/header.js %}
+{%endcomment%}
+{% include js/jobs.js %}
+{% include js/vendor/bootstrap.min.js %}
+{% include js/vendor/jquery.validate.min.js %}
+{%comment%}
+  Temporarily commented out (until we update the theme)
+  {% include js/vendor/openedx/navigation.js %}
+{%endcomment%}
+{% include_relative vendor/prism.min.js %}
