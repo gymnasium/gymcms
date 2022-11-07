6f2c484ba3eab1ce5ebae2c1f77eab7d070490dd
Roman Edirisinghe <rediris@users.noreply.github.com>
2022-11-07 09:39:05 -0500

    adds coffeescript plugin

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source "https://rubygems.org"
 gem "jekyll", "~> 4.1.0"
 
 gem "jekyll-get-json", "~> 1.0"
+
+gem "jekyll-coffeescript"
+
+gem "jekyll-assets", group: :jekyll_plugins
--- js/main.js
+++ js/main.js
@@ -1,20 +1,9 @@
 ---
 ---
-{%comment%}
-Here, we are combining all of our JS files to reduce requests
-TODO: audit these to figure out which we need to load, and where. For example, the prism.js file is only used on quiz/exam pages. Maybe we create an auth.js file, which loads the resources needed for logged in users? Alternately, we pass that file back to the theme.
-{%endcomment%}
-
 {% include js/gymnasium.js %}
-{%comment%}
-  Temporarily commented out (until we update the theme)
-{%endcomment%}
 {% include js/vendor/openedx/header.js %}
 {% include js/jobs.js %}
 {% include js/vendor/bootstrap.min.js %}
 {% include js/vendor/jquery.validate.min.js %}
-{%comment%}
-  Temporarily commented out (until we update the theme)
-{%endcomment%}
 {% include js/vendor/openedx/navigation.js %}
 {% include_relative vendor/prism.min.js %}
