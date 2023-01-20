db55742810ef3fe46da09874bc6b689e5038f6d9
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-01-20 12:13:30 -0500

    Fix broken build?

--- .netlify/functions/og.tsx
+++ .netlify/functions/og.tsx
@@ -79,7 +79,7 @@ async function loadDataFile(id: string) {
     const type = data['course_type'] ? data['course_type'] : null;
     const imgBgColor = data['img_bg_color'] ? data['img_bg_color'] : null;
     const bgColor = data['bg_color'] ? data['bg_color'] : null;
-    const img = data['poster_art'] ? data['poster_art'] : null;
+    const img = data['poster_art'] ? data['poster_art'] : 'none';
     
     // console.log(data);
 
@@ -115,279 +115,288 @@ function aspectRatio(width:number, height:number, newWidth:number) {
 }
 
 export default async function handler(req: Request) {
-  try {
-    const fontData = await font;
+  const fontData = await font;
 
-    // Get the query parameters from the request
-    const url = new URL(req.url);
-    const params:any = new URLSearchParams(url.search);
-    const id = params.get('id') ?? null;
-    let bgColor = params.get('bg') ?? '222';
-    let imgBgColor = params.get('imgbg') ?? '222';
-    const courseNum: any = params.get('courseNum') ?? false;
-    const imgOffset:any = Math.abs(params.get('offset')) ?? 0;
-    let titleFontSize = 50;
-    let footerText = params.get('footer') ?? 'thegymnasium.com';
-    // const pubDate = params.get('pubDate') ?? new Date().toISOString();
-    let imgPath: any;
-    let metaPath: any;
-    let type: any;
-    let metaData: any;
-    let hideFooter: boolean = false;
-    let debug: boolean = false;
-  
-    if (params.has('debug')) {
-      debug = true;
-      params.delete('debug');
+  // Get the query parameters from the request
+  const url = new URL(req.url);
+  const params:any = new URLSearchParams(url.search);
+  const id = params.get('id') ?? null;
+  let bgColor = params.get('bg') ?? '222';
+  let imgBgColor = params.get('imgbg') ?? '222';
+  const courseNum: any = params.get('courseNum') ?? false;
+  const imgOffset:any = Math.abs(params.get('offset')) ?? 0;
+  let titleFontSize = 50;
+  let footerText = params.get('footer') ?? 'thegymnasium.com';
+  // const pubDate = params.get('pubDate') ?? new Date().toISOString();
+  let imgPath: any;
+  let metaPath: any;
+  let type: any;
+  let metaData: any;
+  let hideFooter: boolean = false;
+  let debug: boolean = false;
+
+  if (params.has('debug')) {
+    debug = true;
+    params.delete('debug');
+  }
+
+  // Are there any URL parameters (other than debug [see above]) attached? If not, hide the footer too. This is to display only the Gymnasium logo when visiting /og-image
+  if (params && params.toString().length === 0) {
+    hideFooter = true;
+  }
+
+  // TODO: refactor courseNum to use id
+  // If we have a course number, grab specific data
+  if (courseNum) {
+    // if (courseNum < 100) {
+    //   type = 'gym-shorts';
+    //   imgPath = `/img/course-artwork/svg/gym-${courseNum}.svg`;
+    // } else if (courseNum >= 100 && courseNum < 700) {
+    //   type = 'full';
+    //   imgPath = `/img/course-artwork/svg/gym-${courseNum}.svg`;
+    // } else if (courseNum >= 700 && courseNum < 800) {
+    //   type = 'workshops';
+    //   imgPath = null;
+    // } else if (courseNum >= 5000) {
+    //   type = 'take5';
+    //   imgPath = `/img/take5/posters/gym-${courseNum}.jpg`;
+    // }
+
+    // metaPath = `/courses/${type}/GYM-${courseNum}/meta.md`;
+
+    try {
+      metaData = await loadDataFile(`gym-${courseNum}`);
+      imgPath = metaData.img;
+    } catch(err) {
+      console.error(err);
     }
-  
-    // Are there any URL parameters (other than debug [see above]) attached? If not, hide the footer too. This is to display only the Gymnasium logo when visiting /og-image
-    if (params && params.toString().length === 0) {
-      hideFooter = true;
-    }
-  
-    // TODO: refactor courseNum to use id
-    // If we have a course number, grab specific data
-    if (courseNum) {
-      // if (courseNum < 100) {
-      //   type = 'gym-shorts';
-      //   imgPath = `/img/course-artwork/svg/gym-${courseNum}.svg`;
-      // } else if (courseNum >= 100 && courseNum < 700) {
-      //   type = 'full';
-      //   imgPath = `/img/course-artwork/svg/gym-${courseNum}.svg`;
-      // } else if (courseNum >= 700 && courseNum < 800) {
-      //   type = 'workshops';
-      //   imgPath = null;
-      // } else if (courseNum >= 5000) {
-      //   type = 'take5';
-      //   imgPath = `/img/take5/posters/gym-${courseNum}.jpg`;
-      // }
-  
-      // metaPath = `/courses/${type}/GYM-${courseNum}/meta.md`;
-  
-      try {
-        metaData = await loadDataFile(`gym-${courseNum}`);
+  }
+
+  // TODO: refactor courseNum to use id
+  if (id) {
+    try {
+      metaData = await loadDataFile(id);
+      if (metaData.img) {
         imgPath = metaData.img;
-      } catch(err) {
-        console.error(err);
       }
-    }
-  
-    // TODO: refactor courseNum to use id
-    if (id) {
-      try {
-        metaData = await loadDataFile(id);
-        if (metaData.img) {
-          imgPath = metaData.img;
-        }
-        if (metaData.imgBgColor) {
-          imgBgColor = metaData.imgBgColor;
-        }
-        if (metaData.bgColor) {
-          bgColor = metaData.bgColor;
-        }
-        
-      } catch(err) {
-        console.error(err);
+      if (metaData.imgBgColor) {
+        imgBgColor = metaData.imgBgColor;
       }
+      if (metaData.bgColor) {
+        bgColor = metaData.bgColor;
+      }
+      
+    } catch(err) {
+      console.error(err);
     }
+  }
+
+  // defaults
+  const wrapperWidth = 1200;
+  const wrapperHeight = 628;
+  let imgDisplay = 'none';
+  let imgWidth = 0;
+  let bgImg = '';
+  let bgSize = '';
+  let bgPos = '0 0';
+  let contentAlign = 'center';
+  let contentJustify = 'center';
+  let footerCase = 'initial';
+  let footerColor = 'ff5f14';
+  let footerDisplay = 'flex';
+  let logoDisplay = 'flex';
+  let logoWidth = 834;
+  let headerDisplay = 'none';
+  let headerText = '';
+  let wrapperAlign = 'center';
+  let wrapperJustify = 'center';
+
+  // Allow override of title via url params
+  let title;
+
+  // Permit hiding the footer (?footer=false)
+  if (hideFooter === true) {
+    footerDisplay = 'none';
+  }
+
+  let imgUrl;
+  if (imgPath) {
+    imgUrl = `file://${Deno.cwd()}${imgPath}`;
+  }
   
-    // defaults
-    const wrapperWidth = 1200;
-    const wrapperHeight = 628;
-    let imgDisplay = 'none';
-    let imgWidth = 0;
-    let bgImg = '';
-    let bgSize = '';
-    let bgPos = '0 0';
-    let contentAlign = 'center';
-    let contentJustify = 'center';
-    let footerCase = 'initial';
-    let footerColor = 'ff5f14';
-    let footerDisplay = 'flex';
-    let logoDisplay = 'flex';
-    let logoWidth = 834;
-    let headerDisplay = 'none';
-    let headerText = '';
-    let wrapperAlign = 'center';
-    let wrapperJustify = 'center';
-  
-    // Allow override of title via url params
-    let title;
-  
-    // Permit hiding the footer (?footer=false)
-    if (hideFooter === true) {
-      footerDisplay = 'none';
-    }
-  
-    const imgUrl = `file://${Deno.cwd()}${imgPath}`;
-  
-    if (metaData) {
+  if (metaData) {
+    if (metaData.type) {
       type = metaData.type;
-      title = params.get('title') ?? metaData.ogTitle;
     }
     
-    if (metaData.img) {
-      // General defaults + some take 5 settings
-      bgImg = `url(${imgUrl})`;
-      bgSize = 'initial';
-      wrapperJustify = 'flex-start';
-      wrapperAlign = 'flex-start';
-      imgDisplay = 'flex';
-      contentAlign = 'flex-start';
-      logoWidth = 300;
-  
-      if (type === 'take5') {
-        imgWidth = 320;
-        titleFontSize = 90;
-        footerColor = 'ccc';
-        contentJustify = 'space-between';
-        bgPos = `-${imgOffset}px 0px`;
-        footerCase = 'uppercase';
-        logoDisplay = 'none';
-        headerDisplay = 'flex';
-        headerText = 'Take 5';
-        if (metaData.topic) {
-          footerText = metaData.topic;
-        }
-      } else {
-        // calculate aspect ratio for proportional resizing
-        const iconWidth = 516;
-        let iconHeight = aspectRatio(574,488,iconWidth);
-        bgSize = `${iconWidth}px ${iconHeight}px`;
-        const iconVOffset = (wrapperHeight - iconHeight)/2;
-  
-        imgWidth = iconWidth;
-        bgPos = `0px ${iconVOffset}px`;
-        titleFontSize = 70;
-        contentJustify = 'center';
-      }
+    if (metaData.title) {
+      title = metaData.ogTitle;
     }
-  
-    let contentWidth = wrapperWidth - imgWidth;
-  
-    let CONFIG_WRAPPER = {
-      height: '100%',
-      width: '100%',
-      display: 'flex',
-      flexWrap: 'nowrap',
-      flexDirection: 'row',
-      justifyContent: `${wrapperJustify}`,
-      // alignContent: 'stretch',
-      alignItems: `${wrapperAlign}`,
-      backgroundColor: '#222',
-      color: '#fff',
-      fontSize: '50px',
-      fontWeight: 'bold',
-      fontFamily: 'brandon-grotesque',
-      position: 'relative',
-      zIndex: 1,
-      lineHeight: 1,
-    }
-  
-    let CONFIG_IMG = {
-      display: `${imgDisplay}`,
-      order: 1,
-      flexGrow: 0,
-      flexShrink: 0,
-      alignSelf: 'center',
-      justifyContent: 'center',
-      height: '100%',
-      width: '100%',
-      backgroundImage: `${bgImg}`,
-      backgroundColor: `#${imgBgColor}`,
-      backgroundOrigin: 'border-box',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: `${bgPos}`,
-      backgroundSize: `${bgSize}`,
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      zIndex: 2,
-    }
-  
-    let CONFIG_CONTENT = {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: `${contentJustify}`,
-      alignItems: `${contentAlign}`,
-      padding: '60px',
-      marginLeft: `${imgWidth}px`,
-      backgroundColor: `#${bgColor}`,
-      width: `${contentWidth}px`,
-      height: '100%',
-    }
-  
-    let CONFIG_TITLE = {
-      color: '#fff',
-      textTransform: 'uppercase',
-      fontSize: titleFontSize,
-    }
-  
-    let CONFIG_HEADER = {
-      color: '#000',
-      display: `${headerDisplay}`,
-    }
-  
-    let CONFIG_LOGO = {
-      display: `${logoDisplay}`,
-      width: `${logoWidth}`,
-    }
-  
-    let CONFIG_FOOTER = {
-      color: `#${footerColor}`,
-      textTransform: `${footerCase}`,
-      display: `${footerDisplay}`,
-    }
-  
-    if (debug) {
-      console.log(
-        'URL parameters: ',
-        params.toString(), '\n',
-        'CONFIG_WRAPPER =',
-        CONFIG_WRAPPER, '\n',
-        'CONFIG_IMG =',
-        CONFIG_IMG, '\n',
-        'CONFIG_CONTENT =',
-        CONFIG_CONTENT, '\n',
-        'CONFIG_HEADER =',
-        CONFIG_HEADER, '\n',
-        'CONFIG_LOGO =',
-        CONFIG_LOGO, '\n',
-        'CONFIG_TITLE =',
-        CONFIG_TITLE, '\n',
-        'CONFIG_FOOTER =',
-        CONFIG_FOOTER, '\n',
-      );
-    }
-  
-    // Generate the open graph image
-    return new ImageResponse(
-      (
-        <div style={CONFIG_WRAPPER}>
-          <figure style={CONFIG_IMG}></figure>
-          <section style={CONFIG_CONTENT}>
-            <header style={CONFIG_HEADER}>{headerText}</header>
-            <img style={CONFIG_LOGO} src='https://thegymcms.com/img/brand/svg/gymnasium-logo-white.svg' />
-            <h1 style={CONFIG_TITLE}>{title}</h1>
-            <div style={CONFIG_FOOTER}>{footerText}</div>
-          </section>
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 628,
-        debug: debug,
-        fonts: [
-          {
-            name: 'brandon-grotesque',
-            data: fontData,
-            style: 'normal',
-          }
-        ],
-      }
-    );
-  } catch(err) {
-    console.error(err);
+    
   }
+
+  if (params.get('title')) {
+    title = params.get('title')
+  }
+  
+  if (imgUrl) {
+    // General defaults + some take 5 settings
+    bgImg = `url(${imgUrl})`;
+    bgSize = 'initial';
+    wrapperJustify = 'flex-start';
+    wrapperAlign = 'flex-start';
+    imgDisplay = 'flex';
+    contentAlign = 'flex-start';
+    logoWidth = 300;
+
+    if (type === 'take5') {
+      imgWidth = 320;
+      titleFontSize = 90;
+      footerColor = 'ccc';
+      contentJustify = 'space-between';
+      bgPos = `-${imgOffset}px 0px`;
+      footerCase = 'uppercase';
+      logoDisplay = 'none';
+      headerDisplay = 'flex';
+      headerText = 'Take 5';
+      if (metaData.topic) {
+        footerText = metaData.topic;
+      }
+    } else {
+      // calculate aspect ratio for proportional resizing
+      const iconWidth = 516;
+      let iconHeight = aspectRatio(574,488,iconWidth);
+      bgSize = `${iconWidth}px ${iconHeight}px`;
+      const iconVOffset = (wrapperHeight - iconHeight)/2;
+
+      imgWidth = iconWidth;
+      bgPos = `0px ${iconVOffset}px`;
+      titleFontSize = 70;
+      contentJustify = 'center';
+    }
+  }
+
+  let contentWidth = wrapperWidth - imgWidth;
+
+  let CONFIG_WRAPPER = {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexWrap: 'nowrap',
+    flexDirection: 'row',
+    justifyContent: `${wrapperJustify}`,
+    // alignContent: 'stretch',
+    alignItems: `${wrapperAlign}`,
+    backgroundColor: '#222',
+    color: '#fff',
+    fontSize: '50px',
+    fontWeight: 'bold',
+    fontFamily: 'brandon-grotesque',
+    position: 'relative',
+    zIndex: 1,
+    lineHeight: 1,
+  }
+
+  let CONFIG_IMG = {
+    display: `${imgDisplay}`,
+    order: 1,
+    flexGrow: 0,
+    flexShrink: 0,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+    backgroundImage: `${bgImg}`,
+    backgroundColor: `#${imgBgColor}`,
+    backgroundOrigin: 'border-box',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: `${bgPos}`,
+    backgroundSize: `${bgSize}`,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 2,
+  }
+
+  let CONFIG_CONTENT = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: `${contentJustify}`,
+    alignItems: `${contentAlign}`,
+    padding: '60px',
+    marginLeft: `${imgWidth}px`,
+    backgroundColor: `#${bgColor}`,
+    width: `${contentWidth}px`,
+    height: '100%',
+  }
+
+  let CONFIG_TITLE = {
+    color: '#fff',
+    textTransform: 'uppercase',
+    fontSize: titleFontSize,
+  }
+
+  let CONFIG_HEADER = {
+    color: '#000',
+    display: `${headerDisplay}`,
+  }
+
+  let CONFIG_LOGO = {
+    display: `${logoDisplay}`,
+    width: `${logoWidth}`,
+  }
+
+  let CONFIG_FOOTER = {
+    color: `#${footerColor}`,
+    textTransform: `${footerCase}`,
+    display: `${footerDisplay}`,
+  }
+
+  if (debug) {
+    console.log(
+      'URL parameters: ',
+      params.toString(), '\n',
+      'CONFIG_WRAPPER =',
+      CONFIG_WRAPPER, '\n',
+      'CONFIG_IMG =',
+      CONFIG_IMG, '\n',
+      'CONFIG_CONTENT =',
+      CONFIG_CONTENT, '\n',
+      'CONFIG_HEADER =',
+      CONFIG_HEADER, '\n',
+      'CONFIG_LOGO =',
+      CONFIG_LOGO, '\n',
+      'CONFIG_TITLE =',
+      CONFIG_TITLE, '\n',
+      'CONFIG_FOOTER =',
+      CONFIG_FOOTER, '\n',
+    );
+  }
+
+  // Generate the open graph image
+  return new ImageResponse(
+    (
+      <div style={CONFIG_WRAPPER}>
+        <figure style={CONFIG_IMG}></figure>
+        <section style={CONFIG_CONTENT}>
+          <header style={CONFIG_HEADER}>{headerText}</header>
+          <img style={CONFIG_LOGO} src='https://thegymcms.com/img/brand/svg/gymnasium-logo-white.svg' />
+          <h1 style={CONFIG_TITLE}>{title}</h1>
+          <div style={CONFIG_FOOTER}>{footerText}</div>
+        </section>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 628,
+      debug: debug,
+      fonts: [
+        {
+          name: 'brandon-grotesque',
+          data: fontData,
+          style: 'normal',
+        }
+      ],
+    }
+  );
 }
