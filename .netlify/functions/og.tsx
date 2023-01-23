a1df8d29fd68aadbd3342a80aa378b7f91e8cdba
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-01-23 17:22:34 -0500

    Working with JSON?

--- .netlify/functions/og.tsx
+++ .netlify/functions/og.tsx
@@ -1,166 +1,70 @@
 import React from 'https://esm.sh/react@18.2.0';
 import { ImageResponse } from 'https://deno.land/x/og_edge/mod.ts'
-import { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
-import { basename, dirname } from 'https://deno.land/std/path/mod.ts';
-import { Buffer } from "https://deno.land/std/io/buffer.ts";
-import { readableStreamFromReader } from "https://deno.land/std@0.140.0/streams/conversion.ts";
-
-// import { ProfanityEngine } from 'npm:@coffeeandfun/google-profanity-words';
-
-import {
-  // parse as yamlParse,
-  parseAll as yamlParseAll,
-  // stringify as yamlStringify,
-} from 'https://deno.land/std/encoding/yaml.ts';
-
 
 // $gym-green: #2c9959;
 // $gym-magenta: #d73158;
 // $gym-purple: #764c9f;
 // $gym-teal: #5ca5a0;
 
-const __dirname = new URL('.', import.meta.url).pathname;
-const __root = `${__dirname}../../`;
-
-const fontUrl = new URL(`${__root}fonts/brandon_bld-webfont.woff`, import.meta.url);
-
-const font = fetch(fontUrl).then(
-  (res) => res.arrayBuffer(),
-);
-
-// Get a full file name if we only have the first part
-async function getFileName(currentPath: string, fileFragment: string) {
-
+// Parse Data File
+async function getData(domain: string, id:string) {
   try {
-    for await (const dirEntry of Deno.readDir(currentPath)) {
-      // console.log(dirEntry);
-      const fileName = dirEntry.name.startsWith(fileFragment) &&  dirEntry.isFile ? dirEntry.name : false;
-      
-      if (fileName) {
-        console.log(fileName);
-        return fileName;
+    const JSON = await fetch(`${domain}/feeds/data.json`);
+    const data = await JSON.json();
+    let itemData: any;
+
+    let type = 'courses';
+    // console.log(Object.entries(data));
+
+    if (id.toLowerCase().startsWith('web')) {
+      type = 'webinars';
+    }
+
+    Object.entries(data[type]).map(obj => {
+      const item:any = obj[1];
+
+      if (id.toLowerCase() === item['id'].toLowerCase()) {
+        itemData = item;
       }
-    }
-  } catch(err) {
-    console.error(err);
-  }
-}
+    });
 
-// Parse YAML data file!
-async function loadDataFile(id: string) {
-  // TODO: process incoming ID and determine data file path based on ID
-  const lowerId = id.toLowerCase();
-  const upperId = id.toUpperCase();
-  let pathFragment:any = '';
-  let fullPath:string = '';
-
-  if (lowerId.startsWith('gym')) {
-
-    const checkId = id.match(/\d+/);
-    const numericId = checkId !== null ? parseInt(checkId, 10) : ()=> {return false};
-
-    if (numericId < 700) {
-      pathFragment = 'courses';
-    } else if (numericId >= 700 && numericId < 800) {
-      pathFragment = 'workshops';
-    } else if (numericId >= 5000) {
-      pathFragment = 'take5';
-    }
-
-    fullPath = `${pathFragment}/${upperId}.yml`;
-
-  } else if (lowerId.startsWith('web')) {
-    let fileName = await getFileName('./_data/webinars/', lowerId);
-    fullPath = `webinars/${fileName}`;
-  }
-
-  try {
-    const yaml = yamlParseAll(await Deno.readTextFile(`./_data/${fullPath}`));
-    const data = yaml[0];
-    const title = data['title'] ?? null;
-    const topic = data['topic'] ?? null;
-    const type = data['course_type'] ?? null;
-    const imgBgColor = data['img_bg_color'] ?? null;
-    const bgColor = data['bg_color'] ?? null;
-    const img = data['poster_art'] ?? 'none';
-    
-    // console.log(data);
-
-    return {title, topic, imgBgColor, bgColor, img, type};
-
-  } catch(e: any) {
+    return itemData;
+  } catch(e:any) {
     console.warn(`${e.message}`);
-    return new Response(`Failed loading data file.`, {
+    return new Response(`Couldn't find matching id!`, {
       status: 500,
     });
   }
 }
 
-// TODO: get meta data file from path (for static pages without data files)
-async function loadMetaData(path: string) {
-
-  if (path) {
-    path = path.replace(/['"]+/g, '');
-    const metaPath = `${path}meta.md`;
-    console.log(`metaPath: ${metaPath}`);
-    try {
-      const yaml = yamlParseAll(await Deno.readTextFile(`.${metaPath}`));
-      const data = yaml[0];
-      const title = data['og_title'] ?? null;
-      const topic = data['topic'] ?? null;
-      const type = 'static';
-      const imgBgColor = data['img_bg_color'] ?? null;
-      const bgColor = data['bg_color'] ?? null;
-      const img = data['poster_art'] ?? null;
-      
-      console.log(data);
-  
-      return {title, topic, imgBgColor, bgColor, img, type};
-    } catch(e: any) {
-      console.warn(`${e.message}`);
-      return new Response(`Failed loading meta file.`, {
-        status: 500,
-      });
-    }
-  }
-}
-
-// async function loadMetaTitle(url: string) {
-//   // const resp = await fetch(url);
-//   const resp = await Deno.readFile(`./${url}`);
-//   const html = await resp.text();
-//   const parser = new DOMParser();
-//   const htmlDoc = parser.parseFromString(html, 'text/html');
-//   const metaTitle = htmlDoc.querySelector('[property="og:title"]');
-//   const ogTitle = metaTitle.getAttribute('content');
-//   const topic = metaTitle.getAttribute('data-topic');
-//   return {ogTitle, topic};
-// }
-
 // Calculate aspect ratio of a resized image, assuming one knows the initial dimensions
-function aspectRatio(width:number, height:number, newWidth:number) {
+async function aspectRatio(width:number, height:number, newWidth:number) {
   let ratio = height/width;
 
   return newWidth * ratio; 
 }
 
 export default async function handler(req: Request) {
-  const fontData = await font;
+  const url = new URL(req.url);
+  const domain = url.origin;
+
+  // load webfont
+  const fontData = await fetch(`${domain}/fonts/brandon_bld-webfont.woff`).then(
+    (res) => res.arrayBuffer(),
+  );
 
   // Get the query parameters from the request
-  const url = new URL(req.url);
   const params:any = new URLSearchParams(url.search);
   const id = params.get('id') ?? null;
-  const metaPath = params.get('metapath') ?? null;
   let bgColor = params.get('bg') ?? '222';
   let imgBgColor = params.get('imgbg') ?? '222';
-  const courseNum: any = params.get('courseNum') ?? false;
-  const imgOffset:any = Math.abs(params.get('offset')) ?? 0;
+  let imgOffset:any = Math.abs(params.get('offset')) ?? 0;
   let titleFontSize = 50;
   let footerText = params.get('footer') ?? 'thegymnasium.com';
   // const pubDate = params.get('pubDate') ?? new Date().toISOString();
   let imgPath: any;
   let title: any;
+  let topic: any;
   let type: any;
   let metaData: any;
   let hideFooter: boolean = false;
@@ -177,39 +81,10 @@ export default async function handler(req: Request) {
   }
 
   // TODO: refactor courseNum to use id
-  // If we have a course number, grab specific data
-  if (courseNum) {
-    // if (courseNum < 100) {
-    //   type = 'gym-shorts';
-    //   imgPath = `/img/course-artwork/svg/gym-${courseNum}.svg`;
-    // } else if (courseNum >= 100 && courseNum < 700) {
-    //   type = 'full';
-    //   imgPath = `/img/course-artwork/svg/gym-${courseNum}.svg`;
-    // } else if (courseNum >= 700 && courseNum < 800) {
-    //   type = 'workshops';
-    //   imgPath = null;
-    // } else if (courseNum >= 5000) {
-    //   type = 'take5';
-    //   imgPath = `/img/take5/posters/gym-${courseNum}.jpg`;
-    // }
-
-    // metaPath = `/courses/${type}/GYM-${courseNum}/meta.md`;
-
-    try {
-      metaData = await loadDataFile(`gym-${courseNum}`);
-      imgPath = metaData.img;
-    } catch(e: any) {
-      console.warn(`${e.message}`);
-      return new Response(`Invalid course number (course data not found)`, {
-        status: 500,
-      });
-    }
-  }
-
-  // TODO: refactor courseNum to use id
   if (id) {
     try {
-      metaData = await loadDataFile(id);      
+      metaData = await getData(domain, id);
+      console.log(metaData);
     } catch(e: any) {
       console.warn(`${e.message}`);
       return new Response(`Invalid id (data file not found)`, {
@@ -218,32 +93,28 @@ export default async function handler(req: Request) {
     }
   }
 
-  if (metaPath) {
-    try {
-      metaData = await loadMetaData(metaPath);     
-    } catch(e: any) {
-      console.warn(`${e.message}`);
-      return new Response(`Invalid meta path (file not found)`, {
-        status: 500,
-      });
-    }
-  }
-
   if (metaData) {
     if (metaData.img) {
       imgPath = metaData.img;
+      console.log(imgPath);
     }
-    if (metaData.imgBgColor) {
-      imgBgColor = metaData.imgBgColor;
+    if (metaData.img_bg_color) {
+      imgBgColor = metaData.img_bg_color;
     }
-    if (metaData.bgColor) {
-      bgColor = metaData.bgColor;
+    if (metaData.img_offset) {
+      imgOffset = metaData.img_offset;
+    }
+    if (metaData.bg_color) {
+      bgColor = metaData.bg_color;
     }
     if (metaData.type) {
       type = metaData.type;
     }
     if (metaData.title) {
       title = metaData.title;
+    }
+    if (metaData.topic) {
+      topic = metaData.topic;
     }
   }
 
@@ -252,8 +123,8 @@ export default async function handler(req: Request) {
   const wrapperHeight = 628;
   let imgDisplay = 'none';
   let imgWidth = 0;
-  let bgImg = '';
-  let bgSize = '';
+  let bgImg = 'none';
+  let bgSize = 'contain';
   let bgPos = '0 0';
   let contentAlign = 'center';
   let contentJustify = 'center';
@@ -272,20 +143,20 @@ export default async function handler(req: Request) {
     footerDisplay = 'none';
   }
 
-  let imgUrl;
+  let imgUrl:any;
 
   if (imgPath) {
-    imgUrl = `file://${__root}${imgPath}`;
+    imgUrl = `${domain}${imgPath}`;
   }
-  
+
   if (params.get('title')) {
     title = params.get('title')
   }
   
   if (imgUrl) {
     // General defaults + some take 5 settings
-    bgImg = `url(${imgUrl})`;
-    bgSize = 'initial';
+    bgImg = `url('${imgUrl}')`;
+    bgSize = '100%';
     wrapperJustify = 'flex-start';
     wrapperAlign = 'flex-start';
     imgDisplay = 'flex';
@@ -302,13 +173,13 @@ export default async function handler(req: Request) {
       logoDisplay = 'none';
       headerDisplay = 'flex';
       headerText = 'Take 5';
-      if (metaData.topic) {
-        footerText = metaData.topic;
+      if (topic) {
+        footerText = topic;
       }
     } else {
       // calculate aspect ratio for proportional resizing
       const iconWidth = 516;
-      let iconHeight = aspectRatio(574,488,iconWidth);
+      let iconHeight = await aspectRatio(574,488,iconWidth);
       bgSize = `${iconWidth}px ${iconHeight}px`;
       const iconVOffset = (wrapperHeight - iconHeight)/2;
 
